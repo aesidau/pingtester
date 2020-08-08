c210e4924fb5ead5c49cc624042cc17f55e656dc
Andrew Scott <andrew@aes.id.au>
2020-08-08 15:43:28 +0100

    Fixed some more of the corrupted bits. Moved references so they work in the pingtester/ directory.

--- pingtest.sh
+++ pingtest.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-/home/pi/pingtestbyto.sh eth0 192.168.0.1 >> /home/pi/pinglogs/eth0modem.log
-/home/pi/pingtestbyto.sh wlan0 192.168.0.1 >> /home/pi/pinglogs/wlan0modem.log
-/home/pi/pingtestbyto.sh eth0 58.162.26.201 >> /home/pi/pinglogs/eth0asp.log
-/home/pi/updatepingstatus.sh
+mkdir -p /home/pi/pingtester/pinglogs
+/home/pi/pingtester/pingtestbyto.sh eth0 192.168.0.1 >> /home/pi/pingtester/pinglogs/eth0modem.log
+/home/pi/pingtester/pingtestbyto.sh wlan0 192.168.0.1 >> /home/pi/pingtester/pinglogs/wlan0modem.log
+/home/pi/pingtester/pingtestbyto.sh eth0 58.162.26.201 >> /home/pi/pingtester/pinglogs/eth0asp.log
+/home/pi/pingtester/updatepingstatus.sh
 #echo `/bin/date '+%s'`,`/bin/ping -I eth0 192.168.0.1 -c 4 -w 7 | /bin/grep ^rtt | /bin/sed -e 's/[^=]*= //' -e 's/ ms//' -e 's/\//,/g'` >> /home/pi/pinglogs/eth0modem.log
 #DATE=`/bin/date '+%s'`
 #/bin/ping -I wlan0 192.168.0.1 -c 4 -w 7 > /home/pi/pinglogs/tempwlan0
--- setup-lcd.sh
+++ setup-lcd.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Sets up the LCD touch screen. Note that X should be installed before hand,
+# and that the Raspberry Pi will reboot afterwards.
 cd /home/pi/
 git clone https://github.com/goodtft/LCD-show.git
 cdmod -R 755 LCD-show
