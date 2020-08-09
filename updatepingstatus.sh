4fb741a99deebbde4b44df9f4272af288e0bdc4b
Andrew Scott <andrew@aes.id.au>
2020-08-09 09:10:11 +0100

    Fixed hopefully last corrupt char

--- updatepingstatus.sh
+++ updatepingstatus.sh
@@ -7,20 +7,20 @@ WIFICOUNT=""
 if [ $RECEIVED -eq 0 ]
 then 
   WIFICOL=red
-  WIFICOUNT=`/usr/bin/tail /home/pi/pingtester/pinglogs/wlan0modem.log | /bin/grep -n -m 1 -v ",.,0" | /usr/bin/cut -d : -f 1` mins
-elif [ $RECEIVED ­lt $SENT ]
+  WIFICOUNT=`/usr/bin/tail /home/pi/pingtester/pinglogs/wlan0modem.log | /bin/grep -n -m 1 -v ",.,0" | /usr/bin/cut -d : -f 1`" mins"
+elif [ $RECEIVED -lt $SENT ]
 then WIFICOL=orange
 else WIFICOL=green
 fi
 
 INFO=`/usr/bin/tail -n 1 /home/pi/pingtester/pinglogs/eth0modem.log`
-SENT=`echo "$INFO" | /usr/bin/cut -d "," -f 2`
+SENT=`echo "$INFO" | /usr/bin/cut -d "," -f 2`
 RECEIVED=`echo "$INFO" | /usr/bin/cut -d "," -f 3`
 MODEMCOUNT=""
 if [ $RECEIVED -eq 0 ]
 then 
   MODEMCOL=red
-  MODEMCOUNT=`/usr/bin/tail /home/pi/pingtester/pinglogs/eth0modem.log | /bin/grep -n -m 1 -v ",.,0" | /usr/bin/cut -d : -f 1` mins
+  MODEMCOUNT=`/usr/bin/tail /home/pi/pingtester/pinglogs/eth0modem.log | /bin/grep -n -m 1 -v ",.,0" | /usr/bin/cut -d : -f 1`" mins"
 elif [ $RECEIVED -lt $SENT ]
 then MODEMCOL=orange
 else MODEMCOL=green
@@ -33,7 +33,7 @@ NBNCOUNT=""
 if [ $RECEIVED -eq 0 ]
 then 
   NBNCOL=red
-  NBNCOUNT=`/usr/bin/tail /home/pi/pingtester/pinglogs/eth0asp.log | /bin/grep -n -m 1 -v ",.,0" | /usr/bin/cut -d : -f 1` mins
+  NBNCOUNT=`/usr/bin/tail /home/pi/pingtester/pinglogs/eth0asp.log | /bin/grep -n -m 1 -v ",.,0" | /usr/bin/cut -d : -f 1`" mins"
 elif [ $RECEIVED -lt $SENT ]
 then NBNCOL=orange
 else NBNCOL=green
